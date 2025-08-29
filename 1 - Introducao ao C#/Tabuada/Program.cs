6f98e932eaa4065b60aebe7154e2b8dfe7b5c88e
Mafmerlos <matheusmerlos02@gmail.com>
2025-08-29 14:13:01 -0300

    Adiciona projeto da Tabuada

---
+++ 1 - Introducao ao C#/Tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero que voce deseja a tabuada: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A tabuada do {numero} é: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($" {numero} x {i} = {numero * i}");
+            }
+            Console.WriteLine("\nDigite um ENTER para encerrar o programa");
+            Console.ReadLine();
+        }
+    }
+}
