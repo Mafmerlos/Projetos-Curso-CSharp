2e665750cec5629ba2bd0dbc26757cde0c932316
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-01 09:00:50 -0300

    Adiciona Projeto Inserir nomes em vetor

---
+++ 1 - Introducao ao C#/InserirNomesEmArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InserirNomesEmArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomes = new string[5];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine($"Insira um nome na posição {i + 1}: ");
+                nomes[i] = Console.ReadLine();
+            }
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine($"Nome {i+1}: {nomes[i]}");
+            }
+
+
+        }
+    }
+}
