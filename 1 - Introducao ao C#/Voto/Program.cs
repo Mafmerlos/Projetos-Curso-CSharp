4a17bb8428331e6dcde52986b8f6e6a034af7519
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-04 21:43:30 -0300

    feat: Adiciona projeto Voto

---
+++ 1 - Introducao ao C#/Voto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Em uma eleição presidencial, existem quatro candidatos.Os votos são informados através de códigos.
+ * Os códigos utilizados são:
+    • 1,2,3,4 votos para os respectivos candidatos;
+    • 5 voto nulo; 
+    • 6 voto em branco.
+    Assim, escreva um programa para receber um único voto e imprima: 
+    • Se o usuário votou em um candidato: "Voto contabilizado com sucesso..."
+    • Se o usuário votou nulo: "Voto nulo contabilizado..."
+    • Se o usuário votou em branco: "Voto branco contabilizado..
+Se o voto for diferente de todas as opções acima: "Voto inválido..."
+*/
+
+
+namespace Voto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO realizar código feito em grupo na sala
+        }
+    }
+}
