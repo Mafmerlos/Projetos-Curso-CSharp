d4b8bc5cf907aa6e9284419a799d19d2f4bbd1a1
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-04 21:42:26 -0300

    feat: Adiciona projeto Salario

---
+++ 1 - Introducao ao C#/Salario/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Faça um Programa que pergunte quanto você ganha por hora 
+ e o número de horas trabalhadas no mês. 
+ Calcule e mostre o total do seu salário no referido mês, 
+ sabendo-se que são descontados 11% para o Imposto de Renda, 
+ 8% para o INSS e 5% para o sindicato,
+Assim, o programa de exibir:
+    salário bruto = ganho por hora * horas trabalhadas no mês
+    quanto pagou ao IR
+    quanto pagou ao INSS
+    quanto pagou ao Sindicato
+    Salário líquido = Salário Bruto - Descontos
+
+dessa forma:
++ Salário Bruto : R$  
+- IR (11%) : R$
+- INSS (8%) : R$
+- Sindicato (5%) : R$
+= Salário Líquido : R$
+
+ */
+
+namespace Salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
