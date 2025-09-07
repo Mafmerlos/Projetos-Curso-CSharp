336567ea744a9a6ca013e06db49ef868deed1399
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-06 21:36:47 -0300

    feat: Adiciona programa Antecessor e Sucessor

---
+++ 1 - Introducao ao C#/AntecessorSucessor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+/*
+ * Faça um programa que solicite um número inteiro para o usuário
+ * Calcule e mostre o Sucessor e o Antecessor.
+ */
+
+namespace AntecessorSucessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Seu antecessor é: {numero - 1}"); 
+            Console.WriteLine($"Seu sucessor é: {numero + 1}");
+
+
+        }
+    }
+}
