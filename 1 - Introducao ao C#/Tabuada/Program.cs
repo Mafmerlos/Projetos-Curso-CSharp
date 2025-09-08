88e8b202e039f455d64793e4989c41f7b298fc40
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-08 14:59:28 -0300

    refactor(Tabuada): Altera implementação para versão manual
    Ajuste realizado para seguir o exemplo dado em aula, utilizando apenas Console.WriteLine e sem o uso de laços de repetição.

--- 1 - Introducao ao C#/Tabuada/Program.cs
+++ 1 - Introducao ao C#/Tabuada/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Tabuada
 {
@@ -10,14 +7,30 @@ namespace Tabuada
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite o numero que voce deseja a tabuada: ");
+            Console.WriteLine("Digite o número que voce deseja a tabuada: ");
             int numero = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"A tabuada do {numero} é: ");
-            for (int i = 0; i <= 10; i++)
+            //Resolvendo exericio da forma que foi ensinado até agora
+
+            Console.WriteLine($"{numero} x {1} = {numero * 1}");
+            Console.WriteLine($"{numero} x {2} = {numero * 2}");
+            Console.WriteLine($"{numero} x {3} = {numero * 3}");
+            Console.WriteLine($"{numero} x {4} = {numero * 4}");
+            Console.WriteLine($"{numero} x {5} = {numero * 5}");
+            Console.WriteLine($"{numero} x {6} = {numero * 6}");
+            Console.WriteLine($"{numero} x {7} = {numero * 7}");
+            Console.WriteLine($"{numero} x {8} = {numero * 8}");
+            Console.WriteLine($"{numero} x {9} = {numero * 9}");
+            Console.WriteLine($"{numero} x {10} = {numero * 10}");
+
+
+            /* Resolvendo problema usando laços de repetição:
+              Console.WriteLine($"A tabuada do {numero} é: ");
+            for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine($" {numero} x {i} = {numero * i}");
-            }
+            }*/
+
             Console.WriteLine("\nDigite um ENTER para encerrar o programa");
             Console.ReadLine();
         }
