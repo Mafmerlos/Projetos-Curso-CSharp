9ebecc86dc0ed48b56529994f9f21c48a754d22e
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-09 08:30:47 -0300

    docs(Tabuada): Adiciona comentário explicativo no código

--- 1 - Introducao ao C#/Tabuada/Program.cs
+++ 1 - Introducao ao C#/Tabuada/Program.cs
@@ -10,8 +10,8 @@ namespace Tabuada
             Console.WriteLine("Digite o número que voce deseja a tabuada: ");
             int numero = int.Parse(Console.ReadLine());
 
-            //Resolvendo exericio da forma que foi ensinado até agora
-
+            /*
+                Resolvendo exericio da forma que foi ensinado até agora
             Console.WriteLine($"{numero} x {1} = {numero * 1}");
             Console.WriteLine($"{numero} x {2} = {numero * 2}");
             Console.WriteLine($"{numero} x {3} = {numero * 3}");
@@ -22,14 +22,14 @@ namespace Tabuada
             Console.WriteLine($"{numero} x {8} = {numero * 8}");
             Console.WriteLine($"{numero} x {9} = {numero * 9}");
             Console.WriteLine($"{numero} x {10} = {numero * 10}");
+            */
 
-
-            /* Resolvendo problema usando laços de repetição:
+           // Resolvendo problema usando laços de repetição:
               Console.WriteLine($"A tabuada do {numero} é: ");
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine($" {numero} x {i} = {numero * i}");
-            }*/
+            }
 
             Console.WriteLine("\nDigite um ENTER para encerrar o programa");
             Console.ReadLine();
