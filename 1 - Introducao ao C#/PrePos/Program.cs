2eca786173e342d703c971d8f92a23714e7813f5
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-14 22:20:10 -0300

    feat(PrePos): Adiciona exemplo de pré e pós-incremento

---
+++ 1 - Introducao ao C#/PrePos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrePos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 89;
+
+            int valorPos = contador++;
+            //valorPos = contador;
+            //contador++;
+            Console.WriteLine(contador);
+            Console.WriteLine(valorPos);
+
+            int valorPre = ++contador;
+            //contador++;
+            //valorPre = contador;
+            Console.WriteLine(contador);
+            Console.WriteLine(valorPre);
+
+            valorPre = contador++ * 2 + 28;
+
+            contador--;
+            --contador;
+        }
+    }
+}
