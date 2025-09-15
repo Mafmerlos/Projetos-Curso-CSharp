8475bb84372303dbd33f231ca7cb6386012755c7
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-15 09:03:15 -0300

    feat(Jogo): Melhora a lógica e a interação com o usuário

--- 1 - Introducao ao C#/Jogo/Program.cs
+++ 1 - Introducao ao C#/Jogo/Program.cs
@@ -4,74 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Jogo
+ * 
+ * Soco  - s => 2 pontos
+ * Chute - c => 5 pontos 
+ * Magia - m => 10 pontos
+ * 
+ * Deseja executar um golpe (s-soco c-chute m-magia x-sair)?
+ * 
+ * x => sair
+ * 
+ * Soco(s)  => 10 => 20 pontos
+ * Chute(s) => 3 => 15 pontos
+ * Magia(s) => 4 => 40 pontos
+ * Total de pontos => 75 pontos
+ * 
+ * -- Pressione ENTER para encerrar --
+ */
+
 namespace Jogo
 {
     internal class Program
     {
+        const int VALORPONTOSSOCO = 2, VALORPONTOSCHUTE = 5, VALORPONTOSMAGIA = 10;
         static void Main(string[] args)
         {
-            const string SOCO = "s", CHUTE = "c", MAGIA = "m", SAIR = "x";
-            const int VALORSOCO = 2, VALORCHUTE = 5, VALORMAGIA = 10;
-            string respostaUsuario = "s";
-            int somaSocos = 0, somaChutes = 0, somaMagia = 0;
-            int contadorSocos = 0, contadorChutes = 0, contadorMagia = 0;
+            string entradaUsuario = "",saidaUsuario = "s", golpe = "";
+            int socos = 0, chutes = 0, magias = 0, totalPontosFinal = 0, quantidadePontosSoco = 0,
+                quantidadePontosChute = 0, quantidadePontosMagia = 0;
 
-            while (respostaUsuario == "s")
+            Console.WriteLine("Deseja jogar? (s - sim / n - não)");
+            entradaUsuario = Console.ReadLine().ToLower();
+            if (entradaUsuario != "s")
+                return;
+
+            Console.WriteLine("--- JOGO DE LUTA ---");
+            while (saidaUsuario != "x")
             {
-                Console.WriteLine();
-                Console.WriteLine("     \tLUTINHA DOS CAMPEÕES     ");
-                Console.WriteLine();
-                Console.WriteLine("\t Soco  - s => 2 pontos     ");
-                Console.WriteLine("\t Chute - c => 5 pontos     ");
-                Console.WriteLine("\t Magia - m => 10 pontos    ");
-
-                Console.WriteLine();
-                Console.Write("Deseja executar um golpe (s-soco c-chute m-magia x-sair)?    ");
-                string golpe = Console.ReadLine();
+                Console.WriteLine(" Deseja executar um golpe (s-soco c-chute m-magia x-sair)?");
+                golpe = Console.ReadLine().ToLower();
 
                 switch (golpe)
                 {
-                    case SOCO:
-                        Console.Clear();
-                        Console.WriteLine("\nVocê deu um SOCO\n");
-                        somaSocos += VALORSOCO;
-                        contadorSocos++;
+                    case "s":
+                        socos++;
                         break;
-                    case CHUTE:
-                        Console.Clear();
-                        Console.WriteLine("\nVocê deu um CHUTE\n");
-                        somaChutes += VALORCHUTE;
-                        contadorChutes++;
+                    case "c":
+                        chutes++;
                         break;
-                    case MAGIA:
-                        Console.Clear();
-                        Console.WriteLine("\nVocê usou uma MAGIA\n");
-                        somaMagia += VALORMAGIA;
-                        contadorMagia++;
+                    case "m":
+                        magias++;
                         break;
-                    case SAIR:
-                        Console.Clear();
-                        respostaUsuario = "n";
+                    case "x":
+                        saidaUsuario = golpe;
                         break;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Opção inválida,tente novamente com as opções válidas");
-                        respostaUsuario = "s";
-                        break;
-
                 }
-
+                    
             }
-            Console.Clear();
-            Console.WriteLine();
-            Console.WriteLine($"\tSoco(s)  => {contadorSocos} => {somaSocos} pontos");
-            Console.WriteLine($"\tChute(s)  => {contadorChutes} => {somaChutes} pontos");
-            Console.WriteLine($"\tMagia(s) => {contadorMagia} => {somaMagia} pontos");
-            Console.WriteLine($"\tTotal de pontos => {somaSocos + somaChutes + somaMagia} pontos");
-            Console.WriteLine();
-            Console.WriteLine("Pressione ENTER para finalizar o programa    ");
-            Console.ReadLine();
 
+            quantidadePontosSoco = socos * VALORPONTOSSOCO;
+            quantidadePontosChute = chutes * VALORPONTOSCHUTE;
+            quantidadePontosMagia = magias * VALORPONTOSMAGIA;
+            totalPontosFinal = quantidadePontosSoco + quantidadePontosChute + quantidadePontosMagia;
+
+            Console.WriteLine($"Soco (s) => Quantidade de socos : {socos}  => {quantidadePontosSoco} Pontos");
+            Console.WriteLine($"Chute (s) => Quantidade de chutes : {chutes} => {quantidadePontosChute} Pontos");
+            Console.WriteLine($"Magia (s) => Quantidade de magias : {magias} => {quantidadePontosMagia} Pontos");
+            Console.WriteLine($"Total de Pontos: {totalPontosFinal}");
+            Console.WriteLine("Digite ENTER para sair...");
+            Console.ReadLine();
         }
     }
 }
