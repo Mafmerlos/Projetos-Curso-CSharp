cfccffa079c0e58249f9a7b894c1d0896187f56b
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-14 22:20:03 -0300

    feat(ParesImpares): Adiciona programa que exibe pares e ímpares

---
+++ 1 - Introducao ao C#/ParesImpares/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * Imprimir todos numeros pares entre 0 e 300.
+ * Imprimir todos numeros ímpares entre 0 e 300.
+ */
+
+namespace ParesImpares
+{
+
+    internal class Program
+    {
+        const int NUMEROMAXIMO = 300, NUMEROMINIMO = 1;
+        static void Main(string[] args) {
+
+            Console.WriteLine("Numeros Pares entre 0 e 300: ");
+            for (int i = NUMEROMINIMO; i <= NUMEROMAXIMO; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);   
+                }      
+            }
+            
+            Console.WriteLine("Numeros Impares entre 0 e 300: ");
+            for (int i = NUMEROMINIMO; i <= NUMEROMAXIMO; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Pressione ENTER para encerrar o programa");
+            Console.ReadLine();
+
+        }
+    }
+}
