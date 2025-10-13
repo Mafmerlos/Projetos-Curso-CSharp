0e470dcf809e3e56938a41cc484955670b74c006
Mafmerlos <matheusmerlos02@gmail.com>
2025-10-13 16:18:00 -0300

    feat(OO): Adiciona projetos ECommerce e RollerCoaster

---
+++ 3 - OO/ECommerce/Models/Carrinho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Models
+{
+    internal class Carrinho
+    {
+        public int Id { get; set; }
+        public Models.Cliente Cliente { get; set; }
+        public List<Models.Produto> Produtos { get; }
+    }
+}
