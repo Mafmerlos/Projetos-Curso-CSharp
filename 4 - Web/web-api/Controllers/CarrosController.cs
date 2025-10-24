5ce92d25bbcbf80ba74571d6c171802f3bc0a1f6
Mafmerlos <matheusmerlos02@gmail.com>
2025-10-24 08:03:23 -0300

    feat(web-api): Adiciona Model e Controller para Carros

---
+++ 4 - Web/web-api/Controllers/CarrosController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Http;
+using web_api.Models;
+
+namespace web_api.Controllers
+{
+    //API Carro versão 1 = 2025-10-23 11:33
+    public class CarrosController : ApiController
+    {
+        private static List<Carro> listaCarro = new List<Models.Carro>();
+        private  static int contador = 0;
+
+        // GET: api/Carros
+        public List<Models.Carro> Get()
+        {
+            return listaCarro;
+
+            //List<Carro> listaCarro = new List<Models.Carro>();
+            //Carro c1 = new Carro();
+            //c1.Nome = "Gol";
+            //c1.Valor = 45.000;
+            //listaCarro.Add(c1);
+
+
+           /*for (int i = 0; i < 900; i++)
+            {
+                listaCarro.Add(new Models.Carro());
+            } */
+
+
+        /*for(int i = 0; i < 900; i++)
+            {
+                Models.Carro c = new Models.Carro();
+                c.Nome = $"Carro {i}";
+                listaCarro.Add(c);
+            }
+            
+           
+
+
+            return listaCarro;
+
+            */
+        }
+
+        // GET: api/Carros/5
+        public Models.Carro Get(int id)
+        {
+            foreach(var carro in listaCarro)
+            {
+                if(carro.Id == id)
+                {
+                    return carro;
+                }
+                
+            }
+            return null;
+        }
+
+        // POST: api/Carros
+        public void Post([FromBody] Models.Carro value)
+        {
+            value.Id = ++contador;
+            listaCarro.Add(value);
+                
+        }
+
+        // PUT: api/Carros/5
+        public void Put(int id, [FromBody]Models.Carro value)
+        {
+            foreach (Models.Carro carro in listaCarro)
+            {
+                if (carro.Id == id)
+                {
+                    carro.Nome = value.Nome;
+                    carro.Valor = value.Valor;
+                    break;
+                }
+
+            }
+
+
+        }
+
+        // DELETE: api/Carros/5
+        public void Delete(int id)
+        {
+            foreach(Models.Carro carro in listaCarro)
+            {
+                if(carro.Id == id)
+                {
+                    listaCarro.Remove(carro);
+                    break;
+                }
+            }
+           
+        }
+    }
+}
---
+++ 4 - Web/web-api/Models/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_api.Models
+{
+    public class Carro
+    {
+        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor {  get; set; }
+
+        public Carro() {
+                }
+
+    }
+}
