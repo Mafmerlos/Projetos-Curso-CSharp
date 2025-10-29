70cb65da1e24b9f3f800371341848b9c6b6eb21f
Mafmerlos <matheusmerlos02@gmail.com>
2025-10-29 09:02:56 -0300

    feat(ProdutosController): Implementa estrutura básica e lógica inicial

--- 4 - Web/web-api/Controllers/ProdutosController.cs
+++ 4 - Web/web-api/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,41 +10,62 @@ namespace web_api.Controllers
 {
     public class ProdutosController : ApiController
     {
+        private static List<Models.Produto> produtos = new List<Models.Produto>();
+        private static int contador = 0;
+
         // GET: api/Produtos
         public List<Models.Produto> Get()
-        {
-            Models.Produto p1 = new Models.Produto();
-            p1.Id = 1;
-            p1.Nome = "TV";
-
-            Models.Produto p2 = new Models.Produto();
-
-            List<Models.Produto> produtos = new List<Models.Produto>();
-            produtos.Add(p1);
-            produtos.Add(p2);
-
+        {         
             return produtos;
         }
 
         // GET: api/Produtos/5
-        public string Get(int id)
+        public Models.Produto Get(int id)
         {
-            return "value";
+            foreach (var produto in produtos)
+            {
+                if (produto.Id == id)
+                {
+                    return produto;                                        
+                }
+                
+            }
+            return null;
         }
 
         // POST: api/Produtos
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Models.Produto produto)
         {
+            produto.Id = ++contador;
+            
+            produtos.Add(produto);
         }
 
         // PUT: api/Produtos/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Models.Produto value)
         {
+            foreach(var produto in produtos)
+            {
+                if (produto.Id == id)
+                {
+                    produto.Nome = value.Nome;
+                    break;
+                }
+            }
         }
 
         // DELETE: api/Produtos/5
         public void Delete(int id)
         {
+            foreach (var produto in produtos)
+            {
+                if (produto.Id == id)
+                {
+                    produtos.Remove(produto);
+                    break;
+                }
+            }
+
         }
     }
 }
